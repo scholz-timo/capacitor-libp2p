f7c46f7cdc2ae3f98a26c852f06aba86543559d8
Timo Scholz <timo.scholz@veto.party>
2023-03-07 19:55:24 +0100

    Added transform functions for web version.

--- src/web.ts
+++ src/web.ts
@@ -2,6 +2,8 @@ import { WebPlugin } from '@capacitor/core';
 import type { P2PProviderPlugin } from './definitions';
 import { IGroupFactory, IGroup } from './definition/Group/IGroup';
 import { IConnectionHandler } from './definition/ConnectionHandler/IConnectionHandler';
+import { fromString as uint8ArrayFromString } from 'uint8arrays/from-string';
+import { toString as uint8ArrayToString } from 'uint8arrays/to-string';
 
 export class P2PProviderWeb extends WebPlugin implements P2PProviderPlugin {
   createConnectionHandler(_options: { groups: IGroup[]; addresses: string[]; }): Promise<IConnectionHandler> {
@@ -13,8 +15,8 @@ export class P2PProviderWeb extends WebPlugin implements P2PProviderPlugin {
   }
   readonly transformer = {
     string: {
-      to: () => {throw new Error("Method not implemented.")},
-      from: () => {throw new Error("Method not implemented.")}
+      to: (value: string) => uint8ArrayFromString(value),
+      from: (value: Uint8Array) => uint8ArrayToString(value)
     }
   }
 
