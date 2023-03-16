2b13d2f9dc0c7282bf128e0ca997e5cafe509d80
Timo Scholz <timo.scholz@veto.party>
2023-03-16 13:28:35 +0100

    Tests + missing packages.

---
+++ ts-test/definition/EventHandler/types.test-d.ts
@@ -0,0 +1,38 @@
+
+import { expectNotType, expectType } from 'tsd';
+import { EventParameters, GetTypesForEntry, ReverseMap, UnionToArray } from '../../../src/definition/EventListener/types';
+
+const callbackGenerator = <T>(): T => {
+    return undefined as unknown as any;
+}
+
+enum ExampleEnum {
+    HELLO = 0,
+    WORLD = 2
+};
+
+type ReversedExampleEnum = ReverseMap<typeof ExampleEnum>;
+expectType<{
+    [ExampleEnum.HELLO]: "HELLO",
+    [ExampleEnum.WORLD]: "WORLD"
+}>(callbackGenerator<ReversedExampleEnum>());
+
+
+type AllNumberedEntryTypes = GetTypesForEntry<number, ReversedExampleEnum>;
+expectType<"HELLO"|"WORLD">(callbackGenerator<AllNumberedEntryTypes>());
+
+
+type ForExampleEnumEntryTypes = GetTypesForEntry<ExampleEnum.HELLO, ReversedExampleEnum>;
+expectType<"HELLO">(callbackGenerator<ForExampleEnumEntryTypes>());
+expectNotType<"WORLD">(callbackGenerator<ForExampleEnumEntryTypes>());
+
+type UnionType = 'hello'|'world'|'example';
+type UnionTypeArray = UnionToArray<UnionType>;
+expectType<['example', 'hello', 'world']>(callbackGenerator<UnionTypeArray>());
+
+type MyTypeGroup = {
+    [ExampleEnum.HELLO]: ["test1"],
+    [ExampleEnum.WORLD]: ["test2"]
+}
+
+expectType<["test2"]>(callbackGenerator<EventParameters<ReversedExampleEnum, ExampleEnum.HELLO, MyTypeGroup, undefined>>())
