a0d2eb46037542aa5a0bb2d2bbc083712b4b73aa
Timo Scholz <timo.scholz@veto.party>
2023-03-22 17:00:56 +0100

    Reformatted type test.

--- ts-test/definition/EventHandler/types.test-d.ts
+++ ts-test/definition/EventHandler/types.test-d.ts
@@ -1,38 +1,53 @@
-
 import { expectNotType, expectType } from 'tsd';
-import { EventParameters, GetTypesForEntry, ReverseMap, UnionToArray } from '../../../src/definition/EventListener/types';
+
+import type {
+  EventParameters,
+  GetTypesForEntry,
+  ReverseMap,
+  UnionToArray,
+} from '../../../src/definition/EventListener/types';
 
 const callbackGenerator = <T>(): T => {
-    return undefined as unknown as any;
-}
+  return undefined as unknown as any;
+};
 
 enum ExampleEnum {
-    HELLO = 0,
-    WORLD = 2
-};
+  HELLO = 0,
+  WORLD = 2,
+}
 
 type ReversedExampleEnum = ReverseMap<typeof ExampleEnum>;
 expectType<{
-    [ExampleEnum.HELLO]: "HELLO",
-    [ExampleEnum.WORLD]: "WORLD"
+  [ExampleEnum.HELLO]: 'HELLO';
+  [ExampleEnum.WORLD]: 'WORLD';
 }>(callbackGenerator<ReversedExampleEnum>());
 
-
 type AllNumberedEntryTypes = GetTypesForEntry<number, ReversedExampleEnum>;
-expectType<"HELLO"|"WORLD">(callbackGenerator<AllNumberedEntryTypes>());
+expectType<'HELLO' | 'WORLD'>(callbackGenerator<AllNumberedEntryTypes>());
 
+type ForExampleEnumEntryTypes = GetTypesForEntry<
+  ExampleEnum.HELLO,
+  ReversedExampleEnum
+>;
+expectType<'HELLO'>(callbackGenerator<ForExampleEnumEntryTypes>());
+expectNotType<'WORLD'>(callbackGenerator<ForExampleEnumEntryTypes>());
 
-type ForExampleEnumEntryTypes = GetTypesForEntry<ExampleEnum.HELLO, ReversedExampleEnum>;
-expectType<"HELLO">(callbackGenerator<ForExampleEnumEntryTypes>());
-expectNotType<"WORLD">(callbackGenerator<ForExampleEnumEntryTypes>());
-
-type UnionType = 'hello'|'world'|'example';
+type UnionType = 'hello' | 'world' | 'example';
 type UnionTypeArray = UnionToArray<UnionType>;
 expectType<['example', 'hello', 'world']>(callbackGenerator<UnionTypeArray>());
 
 type MyTypeGroup = {
-    [ExampleEnum.HELLO]: ["test1"],
-    [ExampleEnum.WORLD]: ["test2"]
-}
+  [ExampleEnum.HELLO]: ['test1'];
+  [ExampleEnum.WORLD]: ['test2'];
+};
 
-expectType<["test2"]>(callbackGenerator<EventParameters<ReversedExampleEnum, ExampleEnum.HELLO, MyTypeGroup, undefined>>())
+expectType<['test2']>(
+  callbackGenerator<
+    EventParameters<
+      ReversedExampleEnum,
+      ExampleEnum.HELLO,
+      MyTypeGroup,
+      undefined
+    >
+  >(),
+);
