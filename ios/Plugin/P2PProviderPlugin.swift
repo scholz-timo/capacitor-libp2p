0e9c3689674f72a267fec4e47258c03f33476889
Timo Scholz <timo.scholz@veto.party>
2023-03-22 17:32:35 +0100

    Added stubs to P2PProvider.swift

--- ios/Plugin/P2PProviderPlugin.swift
+++ ios/Plugin/P2PProviderPlugin.swift
@@ -9,7 +9,128 @@ import Capacitor
 public class P2PProviderPlugin: CAPPlugin {
     private let implementation = P2PProvider()
 
-    @objc func echo(_ call: CAPPluginCall) throws {
+    @objc func createGroup(_ call: CAPPluginCall) throws {
+        let value = call.getString("value") ?? ""
+        call.resolve([
+            "value": try implementation.echo(value)
+        ])
+    }
+
+    @objc func dial(_ call: CAPPluginCall) throws {
+        let value = call.getString("value") ?? ""
+        call.resolve([
+            "value": try implementation.echo(value)
+        ])
+    }
+
+    @objc func closeConnection(_ call: CAPPluginCall) throws {
+        let value = call.getString("value") ?? ""
+        call.resolve([
+            "value": try implementation.echo(value)
+        ])
+    }
+
+    @objc func hangUp(_ call: CAPPluginCall) throws {
+        let value = call.getString("value") ?? ""
+        call.resolve([
+            "value": try implementation.echo(value)
+        ])
+    }
+
+    @objc func closeStream(_ call: CAPPluginCall) throws {
+        let value = call.getString("value") ?? ""
+        call.resolve([
+            "value": try implementation.echo(value)
+        ])
+    }
+
+    @objc func getMyConnections(_ call: CAPPluginCall) throws {
+        let value = call.getString("value") ?? ""
+        call.resolve([
+            "value": try implementation.echo(value)
+        ])
+    }
+
+    @objc func getAddresses(_ call: CAPPluginCall) throws {
+        let value = call.getString("value") ?? ""
+        call.resolve([
+            "value": try implementation.echo(value)
+        ])
+    }
+
+    @objc func createVersionHandler(_ call: CAPPluginCall) throws {
+        let value = call.getString("value") ?? ""
+        call.resolve([
+            "value": try implementation.echo(value)
+        ])
+    }
+
+    @objc func createLibP2PInstance(_ call: CAPPluginCall) throws {
+        let value = call.getString("value") ?? ""
+        call.resolve([
+            "value": try implementation.echo(value)
+        ])
+    }
+
+    @objc func destroyLibP2PInstance(_ call: CAPPluginCall) throws {
+        let value = call.getString("value") ?? ""
+        call.resolve([
+            "value": try implementation.echo(value)
+        ])
+    }
+
+    @objc func startLibP2PInstance(_ call: CAPPluginCall) throws {
+        let value = call.getString("value") ?? ""
+        call.resolve([
+            "value": try implementation.echo(value)
+        ])
+    }
+
+    @objc func stopLibP2PInstance(_ call: CAPPluginCall) throws {
+        let value = call.getString("value") ?? ""
+        call.resolve([
+            "value": try implementation.echo(value)
+        ])
+    }
+
+    // TEMP
+
+    @objc func createLibP2PStream(_ call: CAPPluginCall) throws {
+        let value = call.getString("value") ?? ""
+        call.resolve([
+            "value": try implementation.echo(value)
+        ])
+    }
+
+    @objc func sendDataToStream(_ call: CAPPluginCall) throws {
+        let value = call.getString("value") ?? ""
+        call.resolve([
+            "value": try implementation.echo(value)
+        ])
+    }
+
+    @objc func destroyLibP2PStream(_ call: CAPPluginCall) throws {
+        let value = call.getString("value") ?? ""
+        call.resolve([
+            "value": try implementation.echo(value)
+        ])
+    }
+
+    @objc func onVersionHandlerUpdate(_ call: CAPPluginCall) throws {
+        let value = call.getString("value") ?? ""
+        call.resolve([
+            "value": try implementation.echo(value)
+        ])
+    }
+
+    @objc func sendVersionHandlerResponse(_ call: CAPPluginCall) throws {
+        let value = call.getString("value") ?? ""
+        call.resolve([
+            "value": try implementation.echo(value)
+        ])
+    }
+
+    @objc func createLibP2PStreamListener(_ call: CAPPluginCall) throws {
         let value = call.getString("value") ?? ""
         call.resolve([
             "value": try implementation.echo(value)
